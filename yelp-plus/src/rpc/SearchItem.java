52b41b09ffb5d6f1e9389bd0ab617a3640db9ffb
yyao <yaoyuan526@gmail.com>
2017-11-18 05:11:53 -0800

    initial commit

---
+++ yelp-plus/src/rpc/SearchItem.java
@@ -0,0 +1,89 @@
+package rpc;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import db.MySQLConnection;
+import entity.Item;
+import external.YelpAPI;
+
+/**
+ * Servlet implementation class SearchItem
+ */
+@WebServlet("/search")
+public class SearchItem extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchItem() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String userId = request.getParameter("user_id");
+		double lat = Double.parseDouble(request.getParameter("lat"));
+		double lon = Double.parseDouble(request.getParameter("lon"));
+		String term = request.getParameter("term"); // Term can be empty or null.
+
+		MySQLConnection conn = new MySQLConnection();
+		List<Item> items = conn.searchItems(userId, lat, lon, term);
+		List<JSONObject> list = new ArrayList<>();
+
+		try {
+			for (Item item : items) {
+				// Add a thin version of restaurant object
+				JSONObject obj = item.toJSONObject();
+				list.add(obj);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		JSONArray array = new JSONArray(list);
+		RpcHelper.writeJsonArray(response, array);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			JSONObject input = RpcHelper.readJsonObject(request);
+			if (input.has("user_id") && input.has("visited")) {
+				String userId = (String) input.get("user_id");
+				JSONArray array = (JSONArray) input.get("visited");
+				List<String> visitedEvents = new ArrayList<>();
+				for (int i = 0; i < array.length(); i++) {
+					String itemId = (String) array.get(i);
+					visitedEvents.add(itemId);
+				}
+
+				RpcHelper.writeJsonObject(response,
+						new JSONObject().put("status", "OK"));
+			} else {
+				RpcHelper.writeJsonObject(response,
+						new JSONObject().put("status", "InvalidParameter"));
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
